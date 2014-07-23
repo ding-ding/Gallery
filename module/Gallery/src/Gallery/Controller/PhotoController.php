58ef6f28400870038eb1f028e1c53d5530919738
Artyom Endin <artyom.endin@gmail.com>
2014-07-23 15:12:00 +0400

    first commit

---
+++ module/Gallery/src/Gallery/Controller/PhotoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Gallery\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class PhotoController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    public function addAction()
+    {
+        return new ViewModel();
+    }
+
+    public function deleteAction()
+    {
+        return new ViewModel();
+    }
+
+    public function viewAction()
+    {
+        return new ViewModel();
+    }
+
+
+}
+
