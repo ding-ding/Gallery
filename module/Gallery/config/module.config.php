34c9002d837301107b3def7424ebb7fa34cdbb05
Artyom Endin <artyom.endin@gmail.com>
2014-07-23 17:00:41 +0400

    четвертый комит

--- module/Gallery/config/module.config.php
+++ module/Gallery/config/module.config.php
@@ -1,4 +1,48 @@
 <?php
 return [
-    
+    'router' => [
+		'routes' => [
+			'gallery' => [
+				'type' => 'Zend\Mvc\Router\Http\Literal',
+				'options' => [
+					'route' => '/gallery',
+					'defaults' => [
+						'__NAMESPACE__' => 'Gallery\Controller',
+						'controller'  => 'Album',
+						'action'	  => 'index',
+					],
+				],
+				'may_terminate' => true,
+				'child_routes' => [
+					'default' => [
+						'type' => 'Zend\Mvc\Router\Http\Segment',
+						'options' => [
+							'route' => '/[:controller[/:action[/:id]]]',
+							'constraints' => [
+								'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+								'action'	 => '[a-zA-Z][a-zA-Z0-9_-]*',
+								'id'		 => '[0-9]+',
+							],
+							'defaults' => [
+								
+							],
+						],
+					],
+				],
+			],
+		],
+	],
+	
+	'controllers' => [
+		'invokables' => [
+			'Gallery\Controller\Album' => 'Gallery\Controller\AlbumController',
+			'Gallery\Controller\Photo' => 'Gallery\Controller\PhotoController',
+		],
+	],
+	
+	'view_manager' => [
+		'template_path_stack' => [
+			__DIR__ . '/../view',
+		],
+	],
 ];
